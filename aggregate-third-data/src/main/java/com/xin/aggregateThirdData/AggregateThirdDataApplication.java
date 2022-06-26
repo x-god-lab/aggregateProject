7e3ca26e8b9d7362c38f8a37c2ad437f68789189
xin <xw6464918@gmail.com>
2022-06-26 17:18:30 +0800

    增加pgsql数据源

---
+++ aggregate-third-data/src/main/java/com/xin/aggregateThirdData/AggregateThirdDataApplication.java
@@ -0,0 +1,16 @@
+package com.xin.aggregateThirdData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xin
+ */
+@SpringBootApplication
+public class AggregateThirdDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AggregateThirdDataApplication.class, args);
+	}
+
+}
