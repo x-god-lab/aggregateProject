782cd40f3e1aceb9c0599c0a1c3e57766439cde2
xin <xw6464918@gmail.com>
2022-08-14 00:18:51 +0800

    去除配置

--- aggregate-info/src/main/java/com/xin/aggregateInfo/pojo/upload/UploadParams.java
+++ aggregate-info/src/main/java/com/xin/aggregateInfo/pojo/upload/UploadParams.java
@@ -16,23 +16,18 @@ import org.springframework.stereotype.Component;
 public class UploadParams {
 
     @ApiModelProperty("服务器ip或者主机名")
-//    @Value("${ftp:host}")
     private String host;
 
     @ApiModelProperty("ftp端口")
-//    @Value("${ftp:port}")
     private int port;
 
     @ApiModelProperty("ftp使用的用户")
-//    @Value("${ftp:user}")
     private String username;
 
     @ApiModelProperty("账户密码")
-//    @Value("${ftp:password}")
     private String password;
 
     @ApiModelProperty("上传地址")
-//    @Value("${ftp:filePath}")
     private String filePath;
 
 }
